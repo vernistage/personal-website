0457928205ca057297fa9c590284ebd396511f9f
vernistage <sdickers10@gmail.com>
2017-08-06 10:32:14 -0500

    happy with gallery

--- src/grids.js
+++ src/grids.js
@@ -1,27 +1,26 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import ImageGallery from 'react-image-gallery';
-import backBalanceXL from './images/back-balance-XL.jpg';
 
 const GridExampleVerticallyDivided = () => (
   <Grid divided='vertically'>
-    <Grid.Row columns={1}>
+    <Grid.Row relaxed columns={1}>
       <Grid.Column>
-        musings
-        <img src= {backBalanceXL}/>
+        <h2> about </h2>
+        <p style={{textAlign:"left"}}>
+          The joy that comes from producing art and connecting with an audience drives my work. I have a B.A. and M.A. in Art History and spent years studing and working with artists who inspire my creative process. I am always open to creating custom performance experiences and collaborating on new projects.
+        </p>
       </Grid.Column>
     </Grid.Row>
-
     <Grid.Row columns={1}>
       <Grid.Column>
-        gallery
+        <h2> connect </h2>
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row centered columns={1}>
+      <Grid.Column width={6} textAlign='center'>
+        <h2> gallery </h2>
         <MyComponent />
-      </Grid.Column>
-    </Grid.Row>
-
-    <Grid.Row columns={1}>
-      <Grid.Column>
-        connect
       </Grid.Column>
     </Grid.Row>
   </Grid>
@@ -37,16 +36,24 @@ class MyComponent extends React.Component {
 
     const images = [
       {
-        original: <img src= {backBalanceXL}/>,
-        thumbnail: 'images/back_balance_S.jpg',
+        original: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/flag-XL.jpg',
+        thumbnail: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/flag-S.jpg'
       },
       {
-        original: 'http://lorempixel.com/1000/600/nature/2/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+        original: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/trapeze-split-XL.jpg',
+        thumbnail: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/trapeze-split-S.jpg',
       },
       {
-        original: 'http://lorempixel.com/1000/600/nature/3/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+        original: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/rope-split-XL.jpg',
+        thumbnail: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/rope-split-S.jpg',
+      },
+      {
+        original: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/straps-arabesque-XL.jpg',
+        thumbnail: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/straps-arabesque-S.jpg'
+      },
+      {
+        original: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/handstand-XL.jpg',
+        thumbnail: 'https://s3.ca-central-1.amazonaws.com/sarah-aerial-images/handstand-S.jpg'
       }
     ]
 
