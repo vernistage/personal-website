f90f493900c244fff1d95b89fb042fc6a35d96a8
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-09-25 14:44:05 -0500

    necessary formatting

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -15,15 +15,39 @@ class ContactForm extends Component {
     return (
       <Form>
         <Form.Group widths='equal'>
-          <Form.Field id='form-input-control-first-name' control={Input} label='First name' placeholder='First name' />
-          <Form.Field id='form-input-control-last-name' control={Input} label='Last name' placeholder='Last name' />
+          <Form.Field
+            id='form-input-control-first-name'
+            control={Input}
+            label='First name'
+            placeholder='First name' />
+          <Form.Field
+            id='form-input-control-last-name'
+            control={Input}
+            label='Last name'
+            placeholder='Last name' />
         </Form.Group>
         <Form.Group widths='equal'>
-          <Form.Field id='form-input-control-email' control={Input} label='Email' placeholder='your@email.com' />
-          <Form.Field id='form-input-control-email' control={Input} label='Phone number' placeholder='XXX-XXX-XXXX' />
+          <Form.Field
+            id='form-input-control-email'
+            control={Input}
+            label='Email'
+            placeholder='your@email.com' />
+          <Form.Field
+            id='form-input-control-email'
+            control={Input}
+            label='Phone number'
+            placeholder='XXX-XXX-XXXX' />
         </Form.Group>
-        <Form.Field id='form-textarea-control-opinion' control={TextArea} label='Request' placeholder='How can I help you?' />
-        <Form.Field id='form-button-control-public' control={Button} content='Connect' onClick={this.handleButtonClick}  />
+        <Form.Field
+          id='form-textarea-control-opinion'
+          control={TextArea}
+          label='Request'
+          placeholder='How can I help you?' />
+        <Form.Field
+          id='form-button-control-public'
+          control={Button} 
+          content='Connect'
+          onClick={this.handleButtonClick}  />
        </Form>
      )
    }
