741a58c0f8e30d31bcca9530307131a6988d06aa
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-10-31 14:36:45 -0500

    Background image changes depending on button

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import './Home.css';
+import headshot from './../../images/headshot.jpeg'
+import rope from './../../images/rope.jpg';
 
 class Home extends Component {
+  constructor(){
+    	super();
+      this.state = {
+      	code: false,
+        circus: false,
+      }
+    }
+
   codeBackground(e){
     e.preventDefault();
-    console.log('The link was clicked.');
+    this.setState({code: true})
+    console.log('code');
   }
 
   circusBackground(e){
     e.preventDefault();
-    console.log('The link was clicked.');
+    this.setState({circus: true, code: false})
+    console.log('circus');
+  }
+
+  removeBackground(e){
+    e.preventDefault();
+    this.setState({circus: false, code: false})
+    console.log('out');
   }
 
   render() {
+    let imgUrl = ''
+    if (this.state.code){
+      imgUrl = headshot
+    } else if (this.state.circus){
+      imgUrl = rope
+    } else {
+      imgUrl = ''
+    }
     return (
-      <div className='main-buttons'>
+      <div className='main-buttons' style={{backgroundImage: 'url(' + imgUrl + ')'}}>
         <div className='buttons'>
           <a href='/code'>
-            <Button className='menu-button' size='huge' inverted color='red' onMouseEnter={this.codeBackground}>
+            <Button className='menu-button' size='huge' inverted color='red' onMouseEnter={this.codeBackground.bind(this)} onMouseLeave={this.removeBackground.bind(this)}>
               Code
             </Button>
           </a>
           <a href='/circus'>
-            <Button className='menu-button' size='huge' inverted color='red' onMouseEnter={this.circusBackground}>
+            <Button className='menu-button' size='huge' inverted color='red' onMouseEnter={this.circusBackground.bind(this)} onMouseLeave={this.removeBackground.bind(this)}>
               Circus
             </Button>
           </a>
