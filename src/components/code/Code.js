006a91521427e504abaedc00dfb239b9ff182bde
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-11-02 16:40:24 -0500

    finish portfolio page

--- src/components/code/Code.js
+++ src/components/code/Code.js
@@ -1,98 +1,115 @@
-// React
 import React, { Component } from "react";
+import { Image, Card, Icon, Container, Header } from 'semantic-ui-react'
+import localHost from './../../images/home.png';
+import friendConnector from './../../images/friend.png';
+import linker from './../../images/linker.png';
+import bounce from './../../images/blocked.png';
+import today from './../../images/today.png';
 
 class Code extends Component {
   render() {
     return (
-      <div>
-      <div className="ui container">
-    <h1 className="ui dividing header center">
-      Portfolio
-    </h1>
-     <div className="ui two stackable cards">
-      // Localhost
-      <div className="ui card">
-        <div className="ui slide masked reveal image">
-          <img src="../assets/home.png" className="visible content">
-          <img src="../assets/booking.png" className="hidden content">
-        </div>
-        <div className="content">
-          <a className="header" href="https://mysterious-badlands-77198.herokuapp.com/" target="_blank">LocalHost</a>
-          <div className="meta">
-            <span className="date">LocalHost connects area hosts, who specialize in categories like "Art", "Music", or "Food & Drink", to visitors looking for someone to show them around.</span>
-          </div>
-        </div>
-        <div className="extra content">
-          <a href="https://github.com/vernistage/localhost" target="_blank">
-            <i className="github icon"></i>
-            View on Github
-          </a>
-        </div>
-      </div>
-      // End Localhost
-      // Do512
-      <div className="ui card">
-        <div className="ui slide masked reveal big image">
-          <img src="../assets/today.png" className="visible content">
-          <img src="../assets/tomorrow.png" className="hidden content">
-        </div>
-        <div className="content">
-          <p className="header">Austin Events</p>
-          <div className="meta">
-            <span className="date">Displays today"s and tomorrow"s events in Austin, Texas, using the Do512 API.</span>
-          </div>
-        </div>
-        <div className="extra content">
-          <a href="https://github.com/vernistage/do512" target="_blank">
-            <i className="github icon"></i>
-            View on Github
-          </a>
-        </div>
-      </div>
-      // End Do512
-      // Bounce
-      <div className="ui card">
-        <div className="ui slide masked reveal big image">
-          <img src="../assets/input.png" className="visible content">
-          <img src="../assets/blocked.png" className="hidden content">
-        </div>
-        <div className="content">
-          <p className="header">Bounce</p>
-          <div className="meta">
-            <span className="date">App monitors a users Twitter mentions and automatically mutes accounts based on words or phrases blacklisted by user.</span>
-          </div>
-        </div>
-        <div className="extra content">
-          <a href="https://github.com/vernistage/bounce" target="_blank">
-            <i className="github icon"></i>
-            View on Github
-          </a>
-        </div>
-      </div>
-      // End Bounce
-      // Link Modify
-      <div className="ui card">
-        <div className="ui slide masked reveal big image">
-          <img src="../assets/linker.png" className="visible content">
-          <img src="../assets/text.png" className="hidden content">
-        </div>
-        <div className="content">
-          <a className="header" href="https://link-modify.herokuapp.com/" target="_blank">Linker</a>
-          <div className="meta">
-            <span className="date">Users can modify links and text them to themselves. For instructions, see Github below.</span>
-          </div>
-        </div>
-        <div className="extra content">
-          <a href="https://github.com/vernistage/link-modify" target="_blank">
-            <i className="github icon"></i>
-            View on Github
-          </a>
-        </div>
-      </div>
-      // End Link Modify
-    </div>
-  </div>
-      </div>
+      <Container>
+      <Header as='h2' dividing>
+        Portfolio
+      </Header>
+        <Card.Group>
+          <Card>
+            <Image src={localHost} />
+            <Card.Content>
+              <Card.Header>
+                <a href="https://mysterious-badlands-77198.herokuapp.com/" rel="noreferrer noopener" target="_blank">LocalHost</a>
+              </Card.Header>
+              <Card.Description>LocalHost connects area hosts, who specialize in categories like 'Art', 'Music', or 'Food & Drink', to visitors looking for someone to show them around.</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a href="https://github.com/vernistage/localhost" rel="noreferrer noopener" target="_blank">
+                <Icon name="github" />
+                View on Github
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src={friendConnector} />
+            <Card.Content>
+              <Card.Header>
+                <a href="https://friend-connector.herokuapp.com" rel="noreferrer noopener" target="_blank">Friend Connector</a>
+              </Card.Header>
+              <Card.Description>
+                App allows users to enter their name and a website to register. Upon registration, the website is scraped for headings. Users can form friendships and search for new friends by a topic of interest.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a href="https://github.com/vernistage/friend-connector" rel="noreferrer noopener" target="_blank">
+                <Icon name="github" />
+                View on Github
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src={linker} />
+            <Card.Content>
+              <Card.Header>
+                <a href="https://link-modify.herokuapp.com/" rel="noreferrer noopener" target="_blank">Linker</a>
+              </Card.Header>
+              <Card.Description>
+                Users can modify links and text them to themselves. For instructions, see Github below.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a href="https://github.com/vernistage/link-modify" rel="noreferrer noopener" target="_blank">
+                <Icon name="github" />
+                View on Github
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src={today} />
+            <Card.Content>
+              <Card.Header>Austin Events</Card.Header>
+              <Card.Description>
+                Displays today's and tomorrow's events in Austin, Texas, using the Do512 API.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a href="https://github.com/vernistage/do512" rel="noreferrer noopener" target="_blank">
+                <Icon name="github" />
+                View on Github
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src={bounce} />
+            <Card.Content>
+              <Card.Header>Bounce</Card.Header>
+              <Card.Description>
+                App monitors user Twitter mentions and automatically mutes accounts based on words or phrases blacklisted by user.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a href="https://github.com/vernistage/bounce" rel="noreferrer noopener" target="_blank">
+                <Icon name="github" />
+                View on Github
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='' />
+            <Card.Content>
+              <Card.Header>Battleship</Card.Header>
+              <Card.Description>
+                Javascript simulation of the popular game "Battleship".
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a href="https://github.com/vernistage/battleship" rel="noreferrer noopener" target="_blank">
+                <Icon name="github" />
+                View on Github
+              </a>
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </Container>
     );
   }
 }
