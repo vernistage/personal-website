7522f0b465d4a95ed4c03d98531f2116d0604f48
Sarah Colby <vernistage@users.noreply.github.com>
2017-11-02 17:12:02 -0500

    add menu (#15)

--- src/components/code/Code.js
+++ src/components/code/Code.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Image, Card, Icon, Container, Header } from 'semantic-ui-react'
+import CodeMenu from './Menu.js'
 import localHost from './../../images/home.png';
 import friendConnector from './../../images/friend.png';
 import linker from './../../images/linker.png';
@@ -9,107 +10,110 @@ import today from './../../images/today.png';
 class Code extends Component {
   render() {
     return (
-      <Container>
-      <Header as='h2' dividing>
-        Portfolio
-      </Header>
-        <Card.Group>
-          <Card>
-            <Image src={localHost} />
-            <Card.Content>
-              <Card.Header>
-                <a href="https://mysterious-badlands-77198.herokuapp.com/" rel="noreferrer noopener" target="_blank">LocalHost</a>
-              </Card.Header>
-              <Card.Description>LocalHost connects area hosts, who specialize in categories like 'Art', 'Music', or 'Food & Drink', to visitors looking for someone to show them around.</Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a href="https://github.com/vernistage/localhost" rel="noreferrer noopener" target="_blank">
-                <Icon name="github" />
-                View on Github
-              </a>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Image src={friendConnector} />
-            <Card.Content>
-              <Card.Header>
-                <a href="https://friend-connector.herokuapp.com" rel="noreferrer noopener" target="_blank">Friend Connector</a>
-              </Card.Header>
-              <Card.Description>
-                App allows users to enter their name and a website to register. Upon registration, the website is scraped for headings. Users can form friendships and search for new friends by a topic of interest.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a href="https://github.com/vernistage/friend-connector" rel="noreferrer noopener" target="_blank">
-                <Icon name="github" />
-                View on Github
-              </a>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Image src={linker} />
-            <Card.Content>
-              <Card.Header>
-                <a href="https://link-modify.herokuapp.com/" rel="noreferrer noopener" target="_blank">Linker</a>
-              </Card.Header>
-              <Card.Description>
-                Users can modify links and text them to themselves. For instructions, see Github below.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a href="https://github.com/vernistage/link-modify" rel="noreferrer noopener" target="_blank">
-                <Icon name="github" />
-                View on Github
-              </a>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Image src={today} />
-            <Card.Content>
-              <Card.Header>Austin Events</Card.Header>
-              <Card.Description>
-                Displays today's and tomorrow's events in Austin, Texas, using the Do512 API.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a href="https://github.com/vernistage/do512" rel="noreferrer noopener" target="_blank">
-                <Icon name="github" />
-                View on Github
-              </a>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Image src={bounce} />
-            <Card.Content>
-              <Card.Header>Bounce</Card.Header>
-              <Card.Description>
-                App monitors user Twitter mentions and automatically mutes accounts based on words or phrases blacklisted by user.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a href="https://github.com/vernistage/bounce" rel="noreferrer noopener" target="_blank">
-                <Icon name="github" />
-                View on Github
-              </a>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Image src='' />
-            <Card.Content>
-              <Card.Header>Battleship</Card.Header>
-              <Card.Description>
-                Javascript simulation of the popular game "Battleship".
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a href="https://github.com/vernistage/battleship" rel="noreferrer noopener" target="_blank">
-                <Icon name="github" />
-                View on Github
-              </a>
-            </Card.Content>
-          </Card>
-        </Card.Group>
-      </Container>
+      <div>
+        <CodeMenu  />
+        <Container>
+        <Header as='h2' dividing>
+          Portfolio
+        </Header>
+          <Card.Group>
+            <Card>
+              <Image src={localHost} />
+              <Card.Content>
+                <Card.Header>
+                  <a href="https://mysterious-badlands-77198.herokuapp.com/" rel="noreferrer noopener" target="_blank">LocalHost</a>
+                </Card.Header>
+                <Card.Description>LocalHost connects area hosts, who specialize in categories like 'Art', 'Music', or 'Food & Drink', to visitors looking for someone to show them around.</Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a href="https://github.com/vernistage/localhost" rel="noreferrer noopener" target="_blank">
+                  <Icon name="github" />
+                  View on Github
+                </a>
+              </Card.Content>
+            </Card>
+            <Card>
+              <Image src={friendConnector} />
+              <Card.Content>
+                <Card.Header>
+                  <a href="https://friend-connector.herokuapp.com" rel="noreferrer noopener" target="_blank">Friend Connector</a>
+                </Card.Header>
+                <Card.Description>
+                  App allows users to enter their name and a website to register. Upon registration, the website is scraped for headings. Users can form friendships and search for new friends by a topic of interest.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a href="https://github.com/vernistage/friend-connector" rel="noreferrer noopener" target="_blank">
+                  <Icon name="github" />
+                  View on Github
+                </a>
+              </Card.Content>
+            </Card>
+            <Card>
+              <Image src={linker} />
+              <Card.Content>
+                <Card.Header>
+                  <a href="https://link-modify.herokuapp.com/" rel="noreferrer noopener" target="_blank">Linker</a>
+                </Card.Header>
+                <Card.Description>
+                  Users can modify links and text them to themselves. For instructions, see Github below.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a href="https://github.com/vernistage/link-modify" rel="noreferrer noopener" target="_blank">
+                  <Icon name="github" />
+                  View on Github
+                </a>
+              </Card.Content>
+            </Card>
+            <Card>
+              <Image src={today} />
+              <Card.Content>
+                <Card.Header>Austin Events</Card.Header>
+                <Card.Description>
+                  Displays today's and tomorrow's events in Austin, Texas, using the Do512 API.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a href="https://github.com/vernistage/do512" rel="noreferrer noopener" target="_blank">
+                  <Icon name="github" />
+                  View on Github
+                </a>
+              </Card.Content>
+            </Card>
+            <Card>
+              <Image src={bounce} />
+              <Card.Content>
+                <Card.Header>Bounce</Card.Header>
+                <Card.Description>
+                  App monitors user Twitter mentions and automatically mutes accounts based on words or phrases blacklisted by user.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a href="https://github.com/vernistage/bounce" rel="noreferrer noopener" target="_blank">
+                  <Icon name="github" />
+                  View on Github
+                </a>
+              </Card.Content>
+            </Card>
+            <Card>
+              <Image src='' />
+              <Card.Content>
+                <Card.Header>Battleship</Card.Header>
+                <Card.Description>
+                  Javascript simulation of the popular game "Battleship".
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a href="https://github.com/vernistage/battleship" rel="noreferrer noopener" target="_blank">
+                  <Icon name="github" />
+                  View on Github
+                </a>
+              </Card.Content>
+            </Card>
+          </Card.Group>
+        </Container>
+      </div>
     );
   }
 }
---
+++ src/components/code/Menu.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Menu, Icon, Button } from 'semantic-ui-react'
+
+class CodeMenu extends Component {
+  state = { activeItem: 'About Sarah' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div>
+        <Menu pointing secondary>
+          <Menu.Item header name='About Sarah' active={activeItem === 'About Sarah'} onClick={this.handleItemClick}  href="#bio"/>
+          <Menu.Item name='Portfolio' active={activeItem === 'Portfolio'} onClick={this.handleItemClick} href="#portfolio"/>
+          <Menu.Item name='Resume' active={activeItem === 'Resume'} onClick={this.handleItemClick} />
+          <Menu.Menu position='right'>
+            <Menu.Item href='http://www.linkedin.com/in/sarahacolby' target='_blank'>
+              <Button color='linkedin'>
+                <Icon name='linkedin' /> LinkedIn
+              </Button>
+            </Menu.Item>
+            <Menu.Item href='http://www.github.com/vernistage' target='_blank'>
+              <Button color='google plus'>
+                <Icon name='github' /> Github
+              </Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </div>
+    )
+  }
+}
+
+export default CodeMenu
